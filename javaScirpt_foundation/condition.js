50e50a5a04d0ab593522aa632ff9cd271dfaccab
Motilal <122986292+Motilall@users.noreply.github.com>
2024-12-02 23:11:12 +0530

    Add files via upload

---
+++ javaScirpt_foundation/condition.js
@@ -0,0 +1,43 @@
+let num1 = 5
+let num2 = 8
+
+// if(num1 > num2){
+//     console.log("num1 is greater than num2")
+// }else{
+//     console.log("num1 is not greater than num2")
+// }
+
+// check with string 
+let username = "chai"
+let anotherUser = "chai"
+
+// if(username == anotherUser){
+//     console.log("Pick another username")
+// }else{
+//     console.log("you can pick this username")
+// }
+
+// check if a vriable is a number or not
+let score = 44
+// if(typeof score === "number"){
+//     console.log("this is a number");
+// }else{
+//     console.log("no that is not a number")
+// }
+
+// check if the boolean value is true or false
+// let isReady = false
+// if(isReady){
+//     console.log("tea ready")
+// }else{
+//     console.log("not ready")
+// }
+
+// check if array is empty or not
+let item = ["wow"]
+
+if(item.length === 0){
+    console.log("Array is empty")
+}else{
+    console.log("Array is not empty")
+}
